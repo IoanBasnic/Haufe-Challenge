651fc8f631d97a63e004772a56a86678a722cdae
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-15 18:22:43 +0300

    Added react-router-dom

--- mycv/src/App.js
+++ mycv/src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
+      <About/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
