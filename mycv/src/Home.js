49cf9c5960cc0f8ec4297bb4fee21b677ca416f7
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-15 18:15:15 +0300

    Added .js files for my pages

---
+++ mycv/src/Home.js
@@ -0,0 +1,12 @@
+import React from '/react';
+
+function Home() {
+    return(
+        <div>
+            <h1>Haufe Challenge</h1>
+            <h2>My CV</h2>
+        </div>
+    )
+}
+
+export default Home;
---
+++ mycv/src/Projects.js
@@ -0,0 +1,12 @@
+import React from '/react';
+
+function Projects() {
+    return(
+        <div>
+            <h1>Haufe Challenge</h1>
+            <h2>My CV</h2>
+        </div>
+    )
+}
+
+export default Projects;
