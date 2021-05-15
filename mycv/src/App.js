1d89edfe2bea3f7b849a1cc95d6b3f6aa7e074f4
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-15 18:34:46 +0300

    Added navbar

--- mycv/src/App.js
+++ mycv/src/App.js
@@ -6,9 +6,11 @@ import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import {Route, Link} from "react-router-dom";
+import NavBar from "./NavBar";
 function App() {
   return (
     <div className="App">
+        <NavBar/>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
