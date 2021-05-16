7c2b16be2b5e6e36ea56cb10a27d4bbec19940cb
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-16 13:06:28 +0300

    Finished project page

--- mycv/src/Projects.js
+++ mycv/src/Projects.js
@@ -1,30 +1,42 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Projects() {
     return(
         <div className="bef-wrapper">
-            <div className="wrapper">
+            <div className="wrapper wrapper-proj">
 
                 <div className="row">
                     <div className="column">
-                        <h2>Column 1</h2>
-                        <p>Some text..</p>
+                        <h1 className={"projectHeader"}>Personal projects and group project</h1>
                     </div>
-                    <div className="column">
-                        <h2>Column 2</h2>
-                        <p>Some text..</p>
-                    </div>
-
                 </div>
 
                 <div className="row">
                     <div className="column">
-                        <h2>Column 2</h2>
-                        <p>Some text..</p>
+                        <a href={"https://github.com/IoanBasnic/WAD-Project"}><img src="https://i.ibb.co/ThrpkcM/wad.png"/></a>
+                    </div>
+
+                    <div className="column">
+                        <a href={"https://github.com/IoanBasnic/CG-project"}><img src="https://i.ibb.co/3yFMjzw/p2.png"/></a>
                     </div>
                 </div>
 
+                <div className="row">
+                    <div className="column">
+                        <a href={"https://github.com/IoanBasnic/One-man-s-trash-is-another-man-s-treasure"}><img src="https://i.ibb.co/b7dSPWZ/adbd.png"/></a>
+                    </div>
 
+                    <div className="column">
+                        <a href={"https://github.com/paulcaraa/COproj_benchmark"}><img src="https://i.ibb.co/grtJWmr/c0proj.png"/></a>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        <a href={"https://github.com/IoanBasnic/SEFproject"}><img src="https://i.ibb.co/jfK3D0P/sef.png"/></a>
+                    </div>
+                </div>
             </div>
         </div>
     )
